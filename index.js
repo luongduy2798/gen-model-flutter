f5f182e528983f38088a4d5a73aead3377f991fc
luongduy2798 <nhatduy2727@gmail.com>
2023-06-20 20:57:02 +0700

    fixbug

--- index.js
+++ index.js
@@ -35,7 +35,11 @@ rl.question('Please enter entity name: ', (name) => {
     entity += `\n  ${name}({${newOb}});\n\n`
     entity += `  factory ${name}.fromJson(Map<String, dynamic> json) => _$${name}FromJson(json);\n\n`
     entity += `  Map<String, dynamic> toJson() => _$${name}ToJson(this);\n`
-    entity += `}`
+    entity += `}\n\n`
+    entity += `List<${name}> ${name.toLowerCase()}ListFromJson(String str) => List<${name}>.from(json.decode(str).map((x) => ${name}.fromJson(x)));\n\n`
+    entity += `${name} ${name.toLowerCase()}FromJson(String str) => ${name}.fromJson(json.decode(str));\n\n`
+    entity += `String ${name.toLowerCase()}ToJson(${name} data) => json.encode(data.toJson());`
+
     console.log('=========================================');
     console.log('=========================================');
     console.log(entity);
